4d2456968cee7be5a2c04cf8563d095bda8c3538
Drew Stanger <drewstanger95@gmail.com>
2023-08-19 13:23:20 +0100

    Add basic login and register pages

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
+# load the index
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+
+# Login route
+@app.route('/login')
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+# Register route
+@app.route('/register')
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
