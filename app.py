0f87d74d1960925c642ef97a539eb0f0381307a8
Drew Stanger <drewstanger95@gmail.com>
2023-08-25 17:30:59 +0100

    Add users.db and add basic register route

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 import sqlite3
+
 
 app = Flask(__name__)
 
 
-def __init__(self):
-    conn = sqlite3.connect("users.db")
+def create_user_db():
+    conn = sqlite3.connect("/users.db")
     cursor = conn.cursor()
     cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            username TEXT,
-            password TEXT
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
         )
         """
     )
     conn.commit()
     conn.close()
+
+
+if __name__ == "__main__":
+    create_user_db()
+    app.run(debug=True)
 
 
 # load the index
@@ -35,7 +41,23 @@ def login():
 
 
 # Register route
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "GET":
-        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # TODO:Hash password for security reasons
+
+        # Add user to users.db
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            (username, password),
+        )
+        conn.commit()
+        conn.close()
+        return redirect("/login")
+
+    return render_template("register.html")
