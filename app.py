6d5b1018d25c2f329ad057428e3d7e0875223d31
Drew Stanger <drewstanger95@gmail.com>
2023-08-25 11:16:24 +0100

    Add users database

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, sqlite3
 
 app = Flask(__name__)
 
+
+def __init__(self):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            password TEXT
+        )
+        """
+    )
+    conn.commit()
+    conn.close()
+
+
 # load the index
-@app.route('/')
+@app.route("/")
 def index():
     if request.method == "GET":
         return render_template("index.html")
 
+
 # Login route
-@app.route('/login')
+@app.route("/login")
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    
+
+
 # Register route
-@app.route('/register')
+@app.route("/register")
 def register():
     if request.method == "GET":
         return render_template("register.html")
