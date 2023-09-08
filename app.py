3ef39b3c63dd11755043d6fd2b3d0581d1077b90
Drew Stanger <drewstanger95@gmail.com>
2023-09-08 12:03:03 +0100

    Refactor to use SQLAlchemy

--- app.py
+++ app.py
@@ -1,46 +1,49 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from passlib.hash import sha256_crypt
+from os import path
+
 
 # configure app
 app = Flask(__name__)
 app.secret_key = "used for dev"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_status.db"
 Session(app)
+# init db
+db.init_app(app)
 
-def create_user_db():
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL,
-            password TEXT NOT NULL,
-            isAdmin BOOLEAN NOT NULL
-        )
-        """
-    )
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS user_status (
-            urconst INTEGER PRIMARY KEY,
-            status TEXT NOT NULL,
-            reason TEXT NOT NULL,
-            setting_user_id INTEGER NOT NULL
-            setting_username TEXT NOT NULL 
-            FOREIGN KEY (setting_user_id) REFERENCES users(id)
-        )
-        """
-    )
-    conn.commit()
-    conn.close()
+
+#  define db models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    is_admin = db.Column(db.Boolean)
+    set_status = db.relationship("UserStatus")
+
+
+class UserStatus(db.Model):
+    urconst = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(150), nullable=False)
+    reason = db.Column(db.String(150))
+    setting_user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    setting_user_name = db.Column(db.String(20), nullable=False)
+
+
+def create_database():
+    with app.app_context():
+        if not path.exists("/user_status.db"):
+            db.create_all()
+            print("database created")
 
 
 if __name__ == "__main__":
-    create_user_db()
+    create_database()
     app.run(debug=True)
 
 
@@ -59,14 +62,10 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        conn = sqlite3.connect("users.db")
-        cursor = conn.cursor()
-
-        # Fetch the hashed password, using ? prevents SQL injections
-        cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
-        hashed_password = cursor.fetchone()
-
-        conn.close()
+        # Fetch the hashed password
+        hashed_password = (
+            db.session.query(User.password).filter_by(username=username).first()
+        )
 
         # Check if the password provided matches the hash and redirect to the index
         if sha256_crypt.verify(password, hashed_password[0]):
@@ -74,35 +73,20 @@ def login():
             session["name"] = username
             return redirect("/dashboard")
         # TODO: add error messaging, incorrect password? user does not exist?
-    # If request.method !== POST, load login page 
+    # If request.method !== POST, load login page
     return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
     session["name"] = None
     return redirect("/")
 
+
 @app.route("/dashboard")
 def dashboard():
     # TODO DISPLAY THE LIST OF USERS IN THE USER_STATUS TABLE
-    conn = sqlite3.connect("user_status.db")
-    cursor = conn.cursor()
-
-    # Fetch the hashed password, using ? prevents SQL injections
-    data = cursor.execute("SELECT * FROM user_status")
-    conn.close()
-
-    # TODO: table data
-    table_data = []
-    for entry in data:
-        # urconst= entry.urconst
-        # status= entry.status
-        # reason= entry.reason
-        # set_by= entry.set_by
-        # user_id= entry.user_id
-        table_data.append(entry) 
-
-    return render_template("dashboard.html", table_data=table_data)
+    return render_template("dashboard.html")
 
 
 # Register route
@@ -118,14 +102,11 @@ def register():
             error = "You must provide both a username and a password"
             return render_template("register.html", error=error)
 
-        conn = sqlite3.connect("users.db")
-        cursor = conn.cursor()
-
         # Usernames must be unique, check if already exists
-        is_existing = cursor.execute(
-            "SELECT id FROM users WHERE username=?",
-            [username],
+        is_existing = (
+            db.session.query(User.id).filter(User.username == username).first()
         )
+
         if is_existing:
             # Prompt user to use unique username
             error = f"The username '{username}' already exists, try a different one"
@@ -135,13 +116,8 @@ def register():
             hashed_password = sha256_crypt.encrypt(password)
 
             # Add user to users.db
-            cursor.execute(
-                "INSERT INTO users (username, password) VALUES (?, ?)",
-                (username, hashed_password),
-            )
-            conn.commit()
-            conn.close()
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
             return redirect("/login")
-
     return render_template("register.html")
-
